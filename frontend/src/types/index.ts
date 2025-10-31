a645b885003e46d13cc0001ab1dc6bfc81a10707
Akash-Raj9 <gopakash74@gmail.com>
2025-10-31 20:24:12 +0530

    highway project

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,41 @@
+export interface Slot {
+    _id: string;
+    startTime: string;
+    endTime: string;
+    capacity: number;
+    availableSeats: number;
+}
+
+
+export interface Experience {
+    _id: string;
+    name: string;
+    price: string;
+    description?: string;
+    location?: string;
+    imageUrl?: string;
+    slots: Slot[];
+}
+
+
+export interface Booking {
+    _id?: string;
+    experienceId: string;
+    slotId: string;
+    date: string;
+    quantity: number;
+    customerName: string;
+    customerEmail: string;
+    promoCode?: string;
+    pricePaid?: number;
+    totalPaid: number;
+}
+
+
+export interface PromoValidation {
+    valid: boolean;
+    code: string;
+    type: string;
+    value: number;
+    discount: number;
+}
